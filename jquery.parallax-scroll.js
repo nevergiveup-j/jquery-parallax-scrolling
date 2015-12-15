6c16712df89dbbdbd172b8fb2d594a23ea5fa41a
nevergiveup-j <nevergivejun@163.com>
2015-12-15 14:09:17 +0800

    refs toggle page

--- jquery.parallax-scroll.js
+++ jquery.parallax-scroll.js
@@ -17,6 +17,54 @@
 }(function ($) {
     "use strict";
 
+    /**
+     * 工具库
+     * @type {Object}
+     */
+    var Util = {
+        elementStyle: document.createElement('div').style,
+        // 判断浏览器内核类型
+        vendor: function() {
+            var vendors = ['t', 'webkitT', 'MozT', 'msT', 'OT'],
+                transform,
+                i = 0,
+                l = vendors.length;
+
+            for ( ; i < l; i++ ) {
+                transform = vendors[i] + 'ransform';
+                if ( transform in Util.elementStyle ) {
+                    return vendors[i].substr(0, vendors[i].length-1);
+                }
+            }
+
+            return false;
+        },
+        // 判断浏览器来适配css属性值
+        prefixStyle: function(style) {
+            if ( Util.vendor() === false ) return false;
+            if ( Util.vendor() === '' ) return style;
+
+            return Util.vendor() + style.charAt(0).toUpperCase() + style.substr(1);
+        },
+        // 判断是否支持css transform-3d（需要测试下面属性支持）
+        hasPerspective: function(){
+            var ret = Util.prefixStyle('perspective') in Util.elementStyle;
+            if ( ret && 'webkitPerspective' in Util.elementStyle ) {
+                Util.injectStyles('@media (transform-3d),(-webkit-transform-3d){#modernizr{left:9px;position:absolute;height:3px;}}', function( node, rule ) {
+                    ret = node.offsetLeft === 9 && node.offsetHeight === 3;
+                });
+            }
+            return !!ret;
+        },
+        translateZ: function(){
+            if(Util.hasPerspective){
+                return ' translateZ(0)';
+            }else{
+                return '';
+            }
+        }
+    }
+
     // 分享默认配置
     var defaults = {
         // 列表元素
@@ -56,48 +104,20 @@
         }
 
         this.viewHeight = 0;
+        // 当前页
         this.pageActive = 0;
         // 方向
         this.direction = null;
+        // 是否切换页面
+        this.isSwitchPage = false;
 
-
+        this.$wrap
+            .css('position', 'relative')
+            .css(Util.prefixStyle('transform'), 'translate(0, 0)');
 
         this.uploadHeight();
 
         this.bind();
-    };
-
-
-    /**
-     * 事件
-     */
-    Parallax.prototype.bind = function() {
-        var that = this,
-            timer = null;
-
-        this.$win.on('mousewheel.parallax', function(event, delta) {
-            console.log(delta);
-            //event.preventDefault();
-            that.scrollEvent(event);
-        });
-
-        this.$win.on('resize.parallax', function(event) {
-            timer = setTimeout(function() {
-                that.uploadHeight();
-            }, 300);
-        });
-    };
-
-    /**
-     * 滚动事件
-     */
-    Parallax.prototype.scrollEvent = function(event) {
-        var that = this;
-
-        this.direction = (event.deltaY >= 1) ? 'up' : 'down';
-
-        console.log(this.direction);
-
     };
 
     /**
@@ -117,6 +137,66 @@
         this.$list.css({
             height: this.viewHeight
         })
+    };
+
+    /**
+     * 事件
+     */
+    Parallax.prototype.bind = function() {
+        var that = this,
+            timer = null;
+
+        this.$win.on('mousewheel.parallax', function(event, delta) {
+            that.scrollEvent(event);
+        });
+
+        this.$win.on('resize.parallax', function(event) {
+            timer = setTimeout(function() {
+                that.uploadHeight();
+            }, 300);
+        });
+    };
+
+    /**
+     * 滚动事件
+     */
+    Parallax.prototype.scrollEvent = function(event) {
+        var that = this;
+
+        this.direction = (event.deltaY >= 1) ? 'up' : 'down';
+
+        setTimeout(function(){
+            that.togglePage();
+        }, 300)
+
+    };
+
+    /**
+     * 滚动事件
+     */
+    Parallax.prototype.togglePage = function() {
+        var that = this;
+
+        if(this.isSwitchPage){
+            return;
+        }
+
+        that.isSwitchPage = true;
+
+        if(this.direction == 'up'){
+            this.pageActive--;
+        }else{
+            this.pageActive++;
+        }
+
+        var deltaY = -(this.pageActive * this.viewHeight) + 'px';
+
+        console.log(deltaY);
+
+        this.$wrap
+            .css(Util.prefixStyle('transition'), 'all .3s')
+            .css(Util.prefixStyle('transform'), 'translate(0, '+ deltaY +')');
+
     };
 
     $.fn.parallax = function( options ) {
